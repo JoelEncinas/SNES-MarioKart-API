5423bbb885b9d0921d0ac2f866235c67aceb62eb
JoelEncinas <joelencinasmartin@gmail.com>
2023-04-22 03:00:40 +0200

    added html structure for docs

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 10 requests per windowMs
+  max: 30, // limit each IP to 10 requests per windowMs
   message: "Too many requests from this IP, please try again after a minute",
 });
 
